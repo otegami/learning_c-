0703c6f0f9ec169d3109b7f4fdb0c197a10b4534
otegami <a.s.takuya1026@gmail.com>
2023-01-21 11:12:08 +0900

    Finished Chapter1

---
+++ chapter1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// constexpr int get_value()
+// {
+//   return 3;
+// }
+
+int main()
+{
+  // constexpr int value = get_value();
+  // std::cout << "value: " << value << std::endl;
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Number2" << std::endl;
+  return 0;
+}
