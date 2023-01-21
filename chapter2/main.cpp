8b85f6256e24e69e47ecaf72e13941cc23e06b96
otegami <a.s.takuya1026@gmail.com>
2023-01-21 11:21:30 +0900

    Learned Error & Warning

--- chapter2/main.cpp
+++ chapter2/main.cpp
@@ -9,7 +9,14 @@ int main()
 {
   // constexpr int value = get_value();
   // std::cout << "value: " << value << std::endl;
-  std::cout << "Hello World!" << std::endl;
-  std::cout << "Number2" << std::endl;
+  // std::cout << "Number2" << std::endl;
+  // std::cout << "Hello World!" << std::endl;
+
+  // Compile time error
+  // std::cout << "Hello World" << std::endl
+
+  // Run time error & warning
+  // int value = 7 / 0;
+  // std::cout << "Value: " << value << std::endl;
   return 0;
 }
