0b2abf676925d9e2fd45e0963ecadb5f80e03f18
otegami <a.s.takuya1026@gmail.com>
2023-02-06 19:48:58 +0900

    Learned memory_leaks

---
+++ pointers/memory_leaks.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+  // Example1
+  // int *p_number{new int{67}};
+
+  // int number{55};
+
+  // p_number = &number;
+
+  // Example2
+  // int *p_number2{new int(55)};
+
+  // p_number2 = new int{44};
+
+  // Example3
+  // {
+  //   int *p_number3{new int{54}};
+  // }
+  // Memory with int{54} leaked.
+
+  return 0;
+}
