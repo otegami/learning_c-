8f7c766169c54076e4ac2815f3c8f2353d6a958b
otegami <a.s.takuya1026@gmail.com>
2023-02-08 19:00:24 +0900

    Learned pass_by_value

--- functions/pass_by_value.cpp
+++ functions/pass_by_value.cpp
@@ -1,1 +1,20 @@
-void sya_age
+#include <iostream>
+
+void say_age(int age);
+
+int main()
+{
+  int age{28};
+
+  std::cout << "age - before " << age << std::endl;
+  say_age(age);
+  std::cout << "age - after " << age << std::endl;
+
+  return 0;
+}
+
+void say_age(int age)
+{
+  ++age;
+  std::cout << "Hello! You are " << age << " years old." << std::endl;
+}
