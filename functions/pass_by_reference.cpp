d4101cafe3e9d8a8671279b5787523cc975d094f
otegami <a.s.takuya1026@gmail.com>
2023-02-08 19:10:16 +0900

    Learned pass_by_reference

---
+++ functions/pass_by_reference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void say_age(int &age);
+
+int main()
+{
+  int age{23};
+  std::cout << "age - before " << age << std::endl;
+  say_age(age); // dereference
+  std::cout << "age - after " << age << std::endl;
+
+  return 0;
+}
+
+void say_age(int &age)
+{
+  ++(age);
+  std::cout << "Hello! You are " << age << " years old." << std::endl;
+}
