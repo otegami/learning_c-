0b3247c0a6276bf8bd9dff0efe3847f42d8a21b6
otegami <a.s.takuya1026@gmail.com>
2023-02-20 20:08:44 +0900

    Learned building_you_own_concepts

--- concepts/building_you_own_concepts.cpp
+++ concepts/building_you_own_concepts.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <type_traits>
+#include <concepts>
 
 template <typename T>
-concept MyIntegral = std::is_integral_v<T>
+concept MyIntegral = std::is_integral_v<T>;
 
 template <typename T>
 concept Multipliable = requires(T a, T b) {
@@ -15,8 +17,48 @@ concept Incrementable = requires(T a) {
                           a++;
                         };
 
+// template <typename T>
+// concept Incrementable = requires(T a) {
+//                           a += 1;
+//                           ++a;
+//                           a++;
+//                         };
+
+// template <typename T>
+//   requires MyIntegral<T>
+// T add(T a, T b)
+// {
+//   return a + b;
+// }
+
+// template <typename T>
+//   requires MyIntegral<T>
+// T add(T a, T b)
+// {
+//   return a + b;
+// }
+
+// template <typename T>
+// T add(T a, T b)
+//   requires MyIntegral<T>
+// {
+//   return a + b;
+// }
+
+// MyIntegral auto add(MyIntegral auto a, MyIntegral auto b)
+// {
+//   return a + b;
+// }
+
+// template <typename T>
+//   requires Multipliable<T>
+// T add(T a, T b)
+// {
+//   return a + b;
+// }
+
 template <typename T>
-  requires MyIntegral<t>
+  requires Incrementable<T>
 T add(T a, T b)
 {
   return a + b;
@@ -24,5 +66,16 @@ T add(T a, T b)
 
 int main()
 {
+  // int x{6};
+  // int y{7};
+
+  double x{6};
+  double y{7};
+
+  // std::string x{"Hello"};
+  // std::string y{"World"};
+
+  std::cout << "result: " << add(x, y) << std::endl;
+
   return 0;
 }
